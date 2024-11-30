7bed47751da9e62500ba7e062e47b5489c339ef9
wosyh18 <wosyh18@naver.com>
2024-11-30 10:50:06 +0900

    문법 오류 수정

--- week4/퇴사2.py
+++ week4/퇴사2.py
@@ -5,7 +5,7 @@ for i in range (N) :
    T[i], P[i] = map(int,input().split)
    
 dp = [0] * [N+1] #+1은 초기값
-for n in range ( N-1, -1 ,-1) #뒤에서 앞으로 (완료 기준)
+for n in range ( N-1, -1 ,-1) : #뒤에서 앞으로 (완료 기준)
    if n+T[n] <= N : #기간 내 상담 O
       dp[n]=max(dp[n+1], dp[n+T[n]+P[n]])
    else :
