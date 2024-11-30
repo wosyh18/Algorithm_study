7b74a502f6e200d972f1ee5b954abc64b6f37303
wosyh18 <wosyh18@naver.com>
2024-11-30 10:48:43 +0900

    백준 15486 퇴사2

---
+++ week4/퇴사2.py
@@ -0,0 +1,14 @@
+N = int(input())
+T = [0]*N
+P = [0]*N
+for i in range (N) :
+   T[i], P[i] = map(int,input().split)
+   
+dp = [0] * [N+1] #+1은 초기값
+for n in range ( N-1, -1 ,-1) #뒤에서 앞으로 (완료 기준)
+   if n+T[n] <= N : #기간 내 상담 O
+      dp[n]=max(dp[n+1], dp[n+T[n]+P[n]])
+   else :
+      dp[n] = dp[n+1]
+      
+print (dp[0])
